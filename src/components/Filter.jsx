c3050d255f376b2fe9ddcf9ed5fe3a85b6c4ce0b
Fabian Beltran <fabianbeltran1224@gmail.com>
2023-11-03 18:25:06 -0500

    feature: functional side filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,21 +1,69 @@
+import { useState } from 'react';
 import FilterButton from './FilterButton';
 
-export default function Filter({options, title}) {
+export default function Filter({ options, title }) {
+
+    const optionsFilter = Object.entries(options).map(([key, value]) => {
+        return {
+            title: value.title,
+            colorClass: value.colorClass,
+        }
+    })
+
+    const [filterList, setFilterList] = useState([])
+    const [filterOptions, setFilterOptions] = useState(optionsFilter)
+
+    const selectedFilter = (title, colorClass) => {
+        setFilterOptions(filterOptions.filter((option) => option.title !== title))
+        setFilterList([...filterList, { title, colorClass }])
+    }
+
+    const deleteFilter = (title, colorClass) => {
+        setFilterList(filterList.filter((option) => option.title !== title))
+
+        const newOption = { title, colorClass };
+        const newFilterOptions = [...filterOptions, newOption];
+
+        newFilterOptions.sort((a, b) => a.title.localeCompare(b.title));
+
+        setFilterOptions(newFilterOptions);
+    }
+
     return (
         <div className="mb-2">
             <h2 className="mb-2 text-xl font-bold text-gray-700">{title}</h2>
             <hr />
+            <h2 className="mb-2 text-base font-bold text-gray-700">Selected</h2>
+            {filterList.length === 0 &&
+                <>
+                    <p className="mt-2 text-gray-500">No filter selected</p>
+                </>
+            }
+            {filterList.length > 0 ?
+                Object.entries(filterList).map(([key, value]) => {
+                    return (
+                        <div key={key} className="my-2 mr-2.5">
+                            <FilterButton
+                                title={value.title}
+                                colorClass={value.colorClass}
+                                handleClick={deleteFilter}
+                            />
+                        </div>
+                    );
+                }) : null
+            }
             <ul className="flex flex-wrap mt-2">
-                {Object.entries(options).map(([key, value]) => {
+                {Object.entries(filterOptions).map(([key, value]) => {
                     return (
                         <li
                             key={key}
                             className="my-2 mr-2.5"
-                            style={{width: 'calc(50% - 10px)'}}
+                            style={{ width: 'calc(50% - 10px)' }}
                         >
                             <FilterButton
                                 title={value.title}
                                 colorClass={value.colorClass}
+                                handleClick={selectedFilter}
                             />
                         </li>
                     );
--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,13 +1,12 @@
-export default function ({title, colorClass}) {
+export default function ({ title, colorClass, handleClick }) {
     return (
         <>
-            <input type="checkbox" id={title} className="hidden peer" />
-            <label
-                htmlFor={title}
+            <button
+                onClick={() => handleClick(title, colorClass)}
                 className={` ${colorClass} py-1 select-none cursor-pointer rounded-lg border-2 text-gray-500 border-none transition-colors duration-200 ease-in-out inline-block w-full text-center font-bold`}
             >
                 {title}
-            </label>
+            </button>
         </>
     );
 }
