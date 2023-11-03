3ecd44aee989bf74b0a0331e2e6ca9564866d97b
Juan Motta <juanandres.lopez@crehana.com>
2023-11-02 23:39:09 -0500

    chore: prettied tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,64 +6,64 @@ export default {
             colors: {
                 'transparent-25': '#FFFFFF40',
 
-                'normal': '#CEC5B2',
+                normal: '#CEC5B2',
                 'normal-transparent-25': '#CEC5B240',
 
-                'fighting': '#D2807E',
+                fighting: '#D2807E',
                 'fighting-transparent-25': '#D2807E40',
 
-                'flying': '#D0C4E5',
+                flying: '#D0C4E5',
                 'flying-transparent-25': '#D0C4E540',
 
-                'poison': '#A18EC1',
+                poison: '#A18EC1',
                 'poison-transparent-25': '#A18EC140',
 
-                'ground': '#E9C486',
+                ground: '#E9C486',
                 'ground-transparent-25': '#E9C48640',
 
-                'rock': '#987F72',
+                rock: '#987F72',
                 'rock-transparent-25': '#987F7240',
 
-                'bug': '#C7F184',
+                bug: '#C7F184',
                 'bug-transparent-25': '#C7F18440',
 
-                'ghost': '#706C8A',
+                ghost: '#706C8A',
                 'ghost-transparent-25': '#706C8A40',
 
-                'steel': '#D4D4D4',
+                steel: '#D4D4D4',
                 'steel-transparent-25': '#D4D4D440',
 
-                'fire': '#EC807E',
+                fire: '#EC807E',
                 'fire-transparent-25': '#EC807E40',
 
-                'water': '#8BBCF7',
+                water: '#8BBCF7',
                 'water-transparent-25': '#8BBCF740',
 
-                'grass': '#56C48F',
+                grass: '#56C48F',
                 'grass-transparent-25': '#8BCFA840',
 
-                'electric': '#EFDFA7',
+                electric: '#EFDFA7',
                 'electric-transparent-25': '#EFDFA740',
 
-                'psychic': '#D88BA4',
+                psychic: '#D88BA4',
                 'psychic-transparent-25': '#D88BA440',
 
-                'ice': '#A9E0F0',
+                ice: '#A9E0F0',
                 'ice-transparent-25': '#A9E0F040',
 
-                'dragon': '#A67CAC',
+                dragon: '#A67CAC',
                 'dragon-transparent-25': '#A67CAC40',
 
-                'dark': '#8E817B',
+                dark: '#8E817B',
                 'dark-transparent-25': '#8E817B40',
 
-                'fairy': '#E2B1C3',
+                fairy: '#E2B1C3',
                 'fairy-transparent-25': '#E2B1C340',
 
-                'unknown': '#C4C4C4',
+                unknown: '#C4C4C4',
                 'unknown-transparent-25': '#C4C4C440',
 
-                'shadow': '#4A4A4A',
+                shadow: '#4A4A4A',
                 'shadow-transparent-25': '#4A4A4A40',
             },
         },
