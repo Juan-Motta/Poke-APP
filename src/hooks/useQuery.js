bd3fb50295b4d280983cb4b949cdb86a14fae8c2
Juan Motta <juanandres.lopez@crehana.com>
2023-11-03 12:31:01 -0500

    fix: removed bug to cause swr to fetch data twice

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -2,7 +2,10 @@ import useSWR from 'swr';
 import {usePokemonStore} from '../stores/PokemonStore';
 
 export function useQuery(query, fetcher) {
-    const {data, error, isLoading} = useSWR(query, fetcher);
+    const {data, error, isLoading} = useSWR(query, fetcher, {
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+    });
     if (data) {
         const pokemons = usePokemonStore.getState().pokemons;
         data.map(pokemon => {
