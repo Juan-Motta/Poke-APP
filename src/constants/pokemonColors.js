a4608916927b305a6b0ad0fba56dc1afc3db350d
Juan Motta <juanandres.lopez@crehana.com>
2023-11-05 18:05:08 -0500

    chore: added classes to style text based on pokemon colors

--- src/constants/pokemonColors.js
+++ src/constants/pokemonColors.js
@@ -2,42 +2,52 @@ const POKEMON_COLORS = {
     BLACK: {
         title: 'black',
         colorClass: 'shadow',
+        textColorClass: 'text-shadow',
     },
     BLUE: {
         title: 'blue',
         colorClass: 'water',
+        textColorClass: 'text-water',
     },
     BROWN: {
         title: 'brown',
         colorClass: 'rock',
+        textColorClass: 'text-rock',
     },
     GREEN: {
         title: 'green',
         colorClass: 'grass',
+        textColorClass: 'text-grass',
     },
     GRAY: {
         title: 'gray',
         colorClass: 'unknown',
+        textColorClass: 'text-unknown',
     },
     PINK: {
         title: 'pink',
         colorClass: 'fairy',
+        textColorClass: 'text-fairy',
     },
     PURPLE: {
         title: 'purple',
         colorClass: 'poison',
+        textColorClass: 'text-poison',
     },
     RED: {
         title: 'red',
         colorClass: 'fire',
+        textColorClass: 'text-fire',
     },
     WHITE: {
         title: 'white',
         colorClass: 'steel',
+        textColorClass: 'text-steel',
     },
     YELLOW: {
         title: 'yellow',
         colorClass: 'electric',
+        textColorClass: 'text-electric',
     },
 };
 
