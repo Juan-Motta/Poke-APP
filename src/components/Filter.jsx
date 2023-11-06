5a84e37bad4ddec5fc663c0585ccb45a5035c211
Fabian Beltran <fabianbeltran1224@gmail.com>
2023-11-05 23:01:35 -0500

    fix: working filter by type and color

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { usePokemonStore } from '../stores/PokemonStore';
 import FilterButton from './FilterButton';
 
 export default function Filter({ options, title }) {
@@ -13,13 +14,16 @@ export default function Filter({ options, title }) {
     const [filterList, setFilterList] = useState([])
     const [filterOptions, setFilterOptions] = useState(optionsFilter)
 
-const selectedFilter = (title, colorClass) => {
-    setFilterOptions(filterOptions.filter((option) => option.title !== title))
-    setFilterList([...filterList, { title, colorClass }])
-}
+    const selectedFilter = (title, colorClass) => {
+        const newFilterList = [...filterList, { title, colorClass }];
+        setFilterList(newFilterList);
+        setFilterOptions(filterOptions.filter((option) => option.title !== title));
+        updateFilter(newFilterList);
+    }
 
     const deleteFilter = (title, colorClass) => {
-        setFilterList(filterList.filter((option) => option.title !== title))
+        const newFilterList = filterList.filter((option) => option.title !== title);
+        setFilterList(newFilterList);
 
         const newOption = { title, colorClass };
         const newFilterOptions = [...filterOptions, newOption];
@@ -27,6 +31,14 @@ const selectedFilter = (title, colorClass) => {
         newFilterOptions.sort((a, b) => a.title.localeCompare(b.title));
 
         setFilterOptions(newFilterOptions);
+        updateFilter(newFilterList);
+    }
+    function updateFilter(filterList) {
+        let arraySelectedTypes = filterList.map((item) => item.title);
+        const setVariables = usePokemonStore.getState().setVariables;
+        usePokemonStore.setState({ pokemons: [] });
+        if (title === 'Color') setVariables({ where: { pokemon_v2_pokemonspecy: { pokemon_v2_pokemoncolor: { name: { _in: arraySelectedTypes } } } } })
+        if (title === 'Type') setVariables({ where: { pokemon_v2_pokemontypes: { pokemon_v2_type: { name: { _in: arraySelectedTypes } } } } });
     }
 
     return (
@@ -58,7 +70,7 @@ const selectedFilter = (title, colorClass) => {
                     }) : null
                 }
             </ul>
-            <  hr className='mt-2'/>
+            <  hr className='mt-2' />
             <ul className="flex flex-wrap mt-2">
                 {Object.entries(filterOptions).map(([key, value]) => {
                     return (
